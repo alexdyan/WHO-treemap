83e500a14f84a86d168ed7b998fac8c9567e8314
Alexandra D <ady239@nyu.edu>
2021-10-28 19:07:39 -0400

    working nested formatting

--- main.js
+++ main.js
@@ -1,66 +1,69 @@
+import uid from './uid.js';
+
 const width = 1000;
 const height = 600;
 const svg = d3.select("#svg1")
-    .attr("viewBox", [0, 0, width, height]);
+    .attr("viewBox", [0, 0, width, height])
+    .style("font", "10px sans-serif");
 
 async function main() {
-    const data = await d3.json("json/WHO_YLL_Global_2.json");
+    const data = await d3.json("json/WHO_YLL_Global.json");
 
     const treemap = d3.treemap()
     // .tile(d3.treemapResquarify)
     .size([width, height])
-    .padding(2)
+    .paddingOuter(3)
+    .paddingTop(19)
+    .paddingInner(1)
     
     const root = d3.hierarchy(data)
-        .sum((d) => d.value)
+        .sum(d => d.value)
         .sort((a, b) => b.height - a.height || b.value - a.value);
     treemap(root);
 
-    const color = d3.scaleOrdinal()
-        .domain(["Infectious and parasitic diseases", "Noncommunicable diseases", "Injuries"])
-        .range([ "#402D54", "#D18975", "#8FD175"]);
-
-    // const opacity = d3.scaleLinear()
-    //     .domain([0, 6])
-    //     .range([.5,1]);
+    const color = d3.scaleSequential([-2, 6], d3.interpolateGnBu);
         
     const cell = svg.selectAll("g")
-        .data(root.leaves())
-        .enter().append("g")
-        .attr("transform", (d) => "translate(" + d.x0 + "," + d.y0 + ")");
+        .data(d3.group(root, d => d.height))
+        .join("g")
+        .selectAll("g")
+        .data(d => d[1])
+        .join("g")
+        .attr("transform", d => `translate(${d.x0},${d.y0})`);
+    
+    cell.append("title")
+        .text(d => `${d.ancestors().reverse().map(d => d.data.name).join("/")}\n${d3.format(d.value)}`);
     
     cell.append("rect")
-        .attr("width", (d) => d.x1 - d.x0)
-        .attr("height", (d) => d.y1 - d.y0)
-        .style("fill", (d) => color(d.parent.data.name))
-        // .style("opacity", (d) => opacity(d.data.value));
+        .attr("id", d => (d.cellUid = uid("cell")).id)
+        .attr("width", d => d.x1 - d.x0)
+        .attr("height", d => d.y1 - d.y0)
+        .style("fill", d => {
+            console.log(d.height)
+            return color(d.height)})
 
     cell.append("title")
         .text((d) => d.data.name + "\n" + d3.format(d.value));
     
     cell.append("clipPath")
-        .attr("id", (d) => "clip-" + d.data.code)
+        .attr("id", d => (d.clipUid = uid("clip")).id)
         .append("use")
-        .attr("xlink:href", (d) => "#" + d.data.code);
+        .attr("xlink:href", d => d.cellUid.href);
 
     cell.append("text")
-        .attr("clip-path", (d) => "url(#clip-" + d.data.code + ")")
+        .attr("clip-path", d => d.clipUid)
         .selectAll("tspan")
-            .data((d) => d.data.name.split(/(?=[A-Z][^A-Z])/g))
-            .enter().append("tspan")
-            .attr("x", 4)
-            .attr("y", 10)
-            .text((d) => d);
-
-
-    // svg.selectAll("text")
-        // .data(root.leaves())
-        // .join("text")
-        // .attr("x", (d) => d.x0+5)    // +10 to adjust position (more right)
-        // .attr("y", (d) => d.y0+10)    // +20 to adjust position (lower)
-        // .text((d) => d.data.code )
-        // .attr("font-size", "15px")
-        // .attr("fill", "white");
+            .data(d => (d=== root ? d.ancestors().reverse().map(d => d.data.name).join("/") : d.data.name.split(/(?=[A-Z][^A-Z])/g).concat(d3.format(d.value))))
+            .join("tspan")
+            .attr("fill-opacity", (d, i, nodes) => i === nodes.length - 1 ? 0.7 : null)
+            .text(d => d);
+    
+    cell.filter(d => d.children).selectAll("tspan")
+        .attr("dx", 3)
+        .attr("y", 13);
+    cell.filter(d => !d.children).selectAll("tspan")
+        .attr("x", 3)
+        .attr("y", (d, i, nodes) => `${(i === nodes.length - 1) * 0.3 + 1.1 + i * 0.9}em`);
 }
 
 main();
