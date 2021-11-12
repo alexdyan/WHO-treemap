d08938a9e0bca619cb933c956704e38b1e02ed12
Alexandra D <ady239@nyu.edu>
2021-11-12 15:40:07 -0500

    fixed colors and cells display ylls

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ const svg = d3.select("#svg1")
 
 async function main() {
     const data = await d3.json("json/WHO_YLL_Global_mini.json");
+    const totalYLLs = 1706630.49819042;
 
     const treemap = d3.treemap()
-    // .tile(d3.treemapResquarify)
     .size([width, height])
-    .paddingOuter(3)
+    .paddingOuter(4)
     .paddingTop(19)
     .paddingInner(1)
     
@@ -21,7 +21,24 @@ async function main() {
         .sort((a, b) => b.height - a.height || b.value - a.value);
     treemap(root);
 
-    const color = d3.scaleSequential([-1, 5], d3.interpolateGnBu);
+
+    // const color = d3.scaleSequential([-1, 5], d3.interpolateGnBu);
+    const colorRange1 = ['#5765f5', '#e5e9ff']
+    const color1 = d3.scaleLinear()
+        .domain([0, 4])
+        .interpolate(d3.interpolateHcl)
+        .range([d3.hcl(colorRange1[1]), d3.hcl(colorRange1[0])])
+    const colorRange2 = ['#29a5f6', '#dcf7ff']
+    const color2 = d3.scaleLinear()
+        .domain([0, 4])
+        .interpolate(d3.interpolateHcl)
+        .range([d3.hcl(colorRange2[1]), d3.hcl(colorRange2[0])])
+    const colorRange3 = ['#e33674', '#ffe5f2']
+    const color3 = d3.scaleLinear()
+        .domain([0, 4])
+        .interpolate(d3.interpolateHcl)
+        .range([d3.hcl(colorRange3[1]), d3.hcl(colorRange3[0])])
+    
         
     const cell = svg.selectAll("g")
         .data(d3.group(root, d => d.height))
@@ -31,39 +48,58 @@ async function main() {
         .join("g")
         .attr("transform", d => `translate(${d.x0},${d.y0})`);
     
-    cell.append("title")
-        .text(d => `${d.ancestors().reverse().map(d => d.data.name).join("/")}\n${d3.format(d.value)}`);
-    
+    // the cell boxes
     cell.append("rect")
         .attr("id", d => (d.cellUid = uid("cell")).id)
+        // .attr("fill", d => color(d.height))
+        .attr('fill', (d) => {
+            if (getRoot(d, root) == "Communicable, maternal, perinatal and nutritional conditions") {
+                return color1(d.height);
+            }
+            else if (getRoot(d, root) == "Noncommunicable diseases") {
+                return color2(d.height);
+            }
+            else if (getRoot(d, root) == "Injuries") {
+                return color3(d.height);
+            }
+            else {
+                return "#ffffff"
+            }
+        } )
         .attr("width", d => d.x1 - d.x0)
         .attr("height", d => d.y1 - d.y0)
-        .style("fill", d => {
-            console.log(d.height)
-            return color(d.height)})
 
+
+    // hover text
     cell.append("title")
-        .text((d) => d.data.name + "\n" + d3.format(d.value));
+        .text(d => `${d.ancestors().reverse().map(d => d.data.name).join("/")}\n${d3.format(d.value)}`)
+        .style("fill", "yellow")
     
+    // prevents text from overflowing its cell
     cell.append("clipPath")
         .attr("id", d => (d.clipUid = uid("clip")).id)
         .append("use")
         .attr("xlink:href", d => d.cellUid.href);
 
+    // name and info
     cell.append("text")
         .attr("clip-path", d => d.clipUid)
         .selectAll("tspan")
-            .data(d => (d=== root ? d.ancestors().reverse().map(d => d.data.name).join("/") : d.data.name.split(/(?=[A-Z][^A-Z])/g).concat(d3.format(d.value))))
+            .data(d => (d === root ? d.ancestors().reverse().map(d => d.data.name).join("/") : d.data.name.split(/(?=[A-Z][^A-Z])/g).concat(d3.format(".1f")(totalYLLs * (d.value / 100)))))
             .join("tspan")
-            .attr("fill-opacity", (d, i, nodes) => i === nodes.length - 1 ? 0.7 : null)
             .text(d => d);
-    
+
     cell.filter(d => d.children).selectAll("tspan")
         .attr("dx", 3)
         .attr("y", 13);
     cell.filter(d => !d.children).selectAll("tspan")
         .attr("x", 3)
         .attr("y", (d, i, nodes) => `${(i === nodes.length - 1) * 0.3 + 1.1 + i * 0.9}em`);
+}
+
+function getRoot(d, root) {
+    if ((d !== root) && d.parent.data.name) return getRoot(d.parent)
+    else return d.data.name;
 }
 
 main();
